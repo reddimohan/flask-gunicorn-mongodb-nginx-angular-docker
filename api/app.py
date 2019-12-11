050b8f3a39386da9e8b6ab4540beabb1c9fcdd98
MO <reddimohana@gmail.com>
2019-12-11 23:01:14 +0530

    working on mongodb docker image

--- api/app.py
+++ api/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+import os
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_restplus import Api, Resource, fields
+from flask_pymongo import PyMongo
 from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
+
+# MongoDB connection
+app.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+print(os.environ)
+mongo = PyMongo(app)
+db = mongo.db
+print(db)
 CORS(app)
 
 api = Api(app, version='1.0', title='TodoMVC API',
@@ -19,39 +28,6 @@ todo = api.model('Todo', {
 })
 
 
-class TodoDAO(object):
-    def __init__(self):
-        self.counter = 0
-        self.todos = []
-
-    def get(self, id):
-        for todo in self.todos:
-            if todo['id'] == id:
-                return todo
-        api.abort(404, "Todo {} doesn't exist".format(id))
-
-    def create(self, data):
-        todo = data
-        todo['id'] = self.counter = self.counter + 1
-        self.todos.append(todo)
-        return todo
-
-    def update(self, id, data):
-        todo = self.get(id)
-        todo.update(data)
-        return todo
-
-    def delete(self, id):
-        todo = self.get(id)
-        self.todos.remove(todo)
-
-
-DAO = TodoDAO()
-DAO.create({'task': 'Build an API'})
-DAO.create({'task': '?????'})
-DAO.create({'task': 'profit!'})
-
-
 @ns.route('/')
 class TodoList(Resource):
     '''Shows a list of all todos, and lets you POST to add new tasks'''
@@ -59,43 +35,41 @@ class TodoList(Resource):
     @ns.marshal_list_with(todo)
     def get(self):
         '''List all tasks'''
-        return DAO.todos
+        _todos = db.todo.find()
+
+        item = {}
+        data = []
+        for todo in _todos:
+            item = {
+                'id': str(todo['_id']),
+                'todo': todo['todo']
+            }
+            data.append(item)
+
+        return jsonify(
+            status=True,
+            data=data
+        )
 
     @ns.doc('create_todo')
     @ns.expect(todo)
     @ns.marshal_with(todo, code=201)
     def post(self):
         '''Create a new task'''
-        return DAO.create(api.payload), 201
+        data = request.get_json(force=True)
+        item = {
+            'todo': data['todo']
+        }
+        db.todo.insert_one(item)
 
-
-@ns.route('/<int:id>')
-@ns.response(404, 'Todo not found')
-@ns.param('id', 'The task identifier')
-class Todo(Resource):
-    '''Show a single todo item and lets you delete them'''
-    @ns.doc('get_todo')
-    @ns.marshal_with(todo)
-    def get(self, id):
-        '''Fetch a given resource'''
-        return DAO.get(id)
-
-    @ns.doc('delete_todo')
-    @ns.response(204, 'Todo deleted')
-    def delete(self, id):
-        '''Delete a task given its identifier'''
-        DAO.delete(id)
-        return '', 204
-
-    @ns.expect(todo)
-    @ns.marshal_with(todo)
-    def put(self, id):
-        '''Update a task given its identifier'''
-        return DAO.update(id, api.payload)
+        return jsonify(
+            status=True,
+            message='To-do saved successfully!'
+        ), 201
 
 
 if __name__ == '__main__':
-    print(" ** Starting flask app...")
-    app.run(debug=True)
-    print(" ** Started flask app!")
+    ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
+    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
+    app.run(debug=ENVIRONMENT_DEBUG, port=ENVIRONMENT_PORT)
 
